260f9999b0c0101c128ce16d645c1e0654c024d7
AI1411 <goethe0719@gmail.com>
2020-09-14 22:29:50 +0900

    ブロックチェーンを生成して表示するまで

--- blockchain.go
+++ blockchain.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
 //blockを定義
 type Block struct {
-	nonce int
+	nonce        int
 	previousHash string
-	timestamp int64
+	timestamp    int64
 	//スライス型
 	transactions []string
 }
@@ -30,7 +31,39 @@ func (b *Block) Print() {
 	fmt.Printf("previous Hash %s\n", b.previousHash)
 }
 
-func main()  {
-	b := NewBlock(0, "init hash")
-	b.Print()
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+//blockchainを生成し、初期値をいれる
+func NewBlockchain() *Blockchain {
+	bc := new(Blockchain)
+	bc.CreateBlock(0, "Init hash")
+	return bc
+}
+
+//BlockchainのプールにBlockを追加していく処理
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
+//NewBlockchain表示用
+func (bc *Blockchain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Printf("%s\n", strings.Repeat("*", 25))
+}
+
+func main() {
+	blockchain := NewBlockchain()
+	blockchain.Print()
+	blockchain.CreateBlock(5, "hash 1")
+	blockchain.Print()
+	blockchain.CreateBlock(2, "hash 2")
+	blockchain.Print()
 }
